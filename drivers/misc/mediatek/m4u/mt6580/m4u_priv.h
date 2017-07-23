1262775014549996a9a75ff0876cc6fabff029f7
Shangbing Hu <shangbing.hu@mediatek.com>
2017-07-23 13:02:25 +0800

    [ALPS01997258] M4U : jade m4u driver
    [Detail] m4u part in kernel space
    
    [Solution]
    1, modify m4u base, reg, port
    2, add api "m4u_dma_map_area/m4u_dmap_unmap_area"
    
    [Feature] Others
    
    MTK-Commit-Id: 88da4520aa9b405039f77ee7d9ca9f72ce23f5c4
    
    Change-Id: I648e00a0d64028271ba4f9ca73d6b1bff4c7905a
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS01997258

--- drivers/misc/mediatek/m4u/mt6580/m4u_priv.h
+++ drivers/misc/mediatek/m4u/mt6580/m4u_priv.h
@@ -305,6 +305,15 @@ typedef struct {
 	unsigned int mva;
 } M4U_CACHE_STRUCT;
 
+typedef struct _M4U_DMA {
+	M4U_PORT_ID port;
+	M4U_DMA_TYPE eDMAType;
+	M4U_DMA_DIR eDMADir;
+	unsigned long va;
+	unsigned int size;
+	unsigned int mva;
+} M4U_DMA_STRUCT;
+
 /* IOCTL commnad */
 #define MTK_M4U_MAGICNO 'g'
 #define MTK_M4U_T_POWER_ON            _IOW(MTK_M4U_MAGICNO, 0, int)
@@ -334,6 +343,6 @@ typedef struct {
 #define MTK_M4U_T_CONFIG_PORT_ARRAY   _IOW(MTK_M4U_MAGICNO, 26, int)
 #define MTK_M4U_T_CONFIG_MAU          _IOW(MTK_M4U_MAGICNO, 27, int)
 #define MTK_M4U_T_CONFIG_TF           _IOW(MTK_M4U_MAGICNO, 28, int)
-
+#define MTK_M4U_T_DMA_OP              _IOW(MTK_M4U_MAGICNO, 29, int)
 
 #endif
