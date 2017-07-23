b72a009aeda0c870a9e50c42bc8d9e938e12b844
Shangbing Hu <shangbing.hu@mediatek.com>
2017-07-24 01:49:25 +0800

    [ALPS02127423] ncp1854: Add MTK license statement
    Add MTK copyright&GPLv2 license statement at the top of file.
    
    MTK-Commit-Id: 853211aecc81e33eaf107d9014fcb9e70def3757
    
    Change-Id: I0fb8bca751c9ccf9b41ba8e7c20ec19dd53c75a8
    Signed-off-by: Shangbing Hu <shangbing.hu@mediatek.com>
    CR-Id: ALPS02127423
    Feature: Power Management

--- drivers/misc/mediatek/power/mt6580/ncp1854.h
+++ drivers/misc/mediatek/power/mt6580/ncp1854.h
@@ -1,21 +1,15 @@
-/*****************************************************************************
-*
-* Filename:
-* ---------
-*   ncp1854.h
-*
-* Project:
-* --------
-*   Android
-*
-* Description:
-* ------------
-*   ncp1854 header file
-*
-* Author:
-* -------
-*
-****************************************************************************/
+/*
+ * Copyright (C) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See http://www.gnu.org/licenses/gpl-2.0.html for more details.
+ */
 
 #ifndef _NCP1854_SW_H_
 #define _NCP1854_SW_H_
