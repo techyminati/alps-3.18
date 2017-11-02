90dc05f5503b585fe576b13a98c0b62f0478145c
ZH Chen <zh.chen@mediatek.com>
2017-11-02 14:38:06 +0800

    [ALPS03373988] touch: Fix a bug of writing out of bounds
    [Detail]
    According to the definition in mstart_drv_mutual_test.h,
    ANA4_MUTUAL_CSUB_NUMBER should be equal to 408,
    which causes the arrary szDbBusTxData Writed out of bounds,
    and the macro definition in the header file also does not match the corresponding
    comment.
    [Solution]
    Modify the macro definition in the header file.
    
    MTK-Commit-Id: 46bcf9ab2359ccb3c68c851979accd4fef4a58eb
    
    Change-Id: I4eb4d69da28fce394afdebdfce3111faf0b99de7
    Signed-off-by: ZH Chen <zh.chen@mediatek.com>
    CR-Id: ALPS03373988
    Feature: [Android Default] CapTouch

--- drivers/input/touchscreen/mediatek/msg2238a/mstar_drv_mutual_mp_test.h
+++ drivers/input/touchscreen/mediatek/msg2238a/mstar_drv_mutual_mp_test.h
@@ -10,8 +10,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
-/**
  *
  * @file    mstar_drv_mutual_mp_test.h
  *
@@ -37,16 +35,16 @@
 /*--------------------------------------------------------------------------*/
 
 #define MAX_CHANNEL_NUM  38
-#define MAX_CHANNEL_DRV  30
-#define MAX_CHANNEL_SEN  20
+#define MAX_CHANNEL_DRV  28
+#define MAX_CHANNEL_SEN  14
 #define MAX_MUTUAL_NUM  (MAX_CHANNEL_DRV * MAX_CHANNEL_SEN)
-#define ANA3_MUTUAL_CSUB_NUMBER (192) //192 = 14 * 13 + 10
-#define ANA4_MUTUAL_CSUB_NUMBER (MAX_MUTUAL_NUM - ANA3_MUTUAL_CSUB_NUMBER) //200 = 392 - 192
-#define FILTER1_MUTUAL_DELTA_C_NUMBER (190) //190 = (6 * 14 + 11) * 2
-#define FILTER2_MUTUAL_DELTA_C_NUMBER (594) //594 = (MAX_MUTUAL_NUM - (6 * 14 + 11)) * 2
+#define ANA3_MUTUAL_CSUB_NUMBER (192) /* 192 = 14 * 13 + 10 */
+#define ANA4_MUTUAL_CSUB_NUMBER (MAX_MUTUAL_NUM - ANA3_MUTUAL_CSUB_NUMBER) /* 200 = 392 - 192 */
+#define FILTER1_MUTUAL_DELTA_C_NUMBER (190) /* 190 = (6 * 14 + 11) * 2 */
+#define FILTER2_MUTUAL_DELTA_C_NUMBER (594) /* 594 = (MAX_MUTUAL_NUM - (6 * 14 + 11)) * 2 */
 
 #define FIR_THRESHOLD    6553
-#define FIR_RATIO    50 //25
+#define FIR_RATIO    50
 
 #define IIR_MAX		32600
 #define SHORT_VALUE	2000
@@ -54,7 +52,6 @@
 
 #define CTP_MP_TEST_RETRY_COUNT (3)
 
-//Msg28xx Threshold
 #define SHORTVALUE  3000
 #define WATERVALUE 20000
 #define DC_RANGE  30
@@ -69,11 +66,10 @@
 /* DATA TYPE DEFINITION                                                     */
 /*--------------------------------------------------------------------------*/
 
-typedef struct
-{
-    u8 nMy;
-    u8 nMx;
-    u8 nKeyNum;
+typedef struct {
+	u8 nMy;
+	u8 nMx;
+	u8 nKeyNum;
 } TestScopeInfo_t;
 
 /*--------------------------------------------------------------------------*/
@@ -92,7 +88,7 @@ extern s32 DrvMpTestGetTestResult(void);
 extern void DrvMpTestGetTestScope(TestScopeInfo_t *pInfo);
 extern void DrvMpTestScheduleMpTestWork(ItoTestMode_e eItoTestMode);
 
-#endif //CONFIG_ENABLE_ITO_MP_TEST
-#endif //CONFIG_ENABLE_TOUCH_DRIVER_FOR_MUTUAL_IC
+#endif /* CONFIG_ENABLE_ITO_MP_TEST */
+#endif /* CONFIG_ENABLE_TOUCH_DRIVER_FOR_MUTUAL_IC */
 
 #endif  /* __MSTAR_DRV_MUTUAL_MP_TEST_H__ */
