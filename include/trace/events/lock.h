7210308e2acb13180dea6c2129a7e8e9f6ff94d2
Andress Kuo <andress.kuo@mediatek.com>
2017-11-11 12:08:15 +0800

    [ALPS03594010] ftrace: access an unused string
    [Detail] mutexs name is never to be used and alloc, shall not use it as mutexs name to print.
    
    [Solution] use dep_maps name to instead.
    
    MTK-Commit-Id: b2a47aa6f9869dbc4a7f9ded2638550b76112e4d
    
    Change-Id: I0e5c40a7287553ed10b0a50ed512235196c5939f
    CR-Id: ALPS03594010
    Feature: ftrace
    Signed-off-by: Andress Kuo <andress.kuo@mediatek.com>

--- include/trace/events/lock.h
+++ include/trace/events/lock.h
@@ -15,16 +15,24 @@ TRACE_EVENT(mutex_contended,
 	TP_ARGS(lock, ip),
 
 	TP_STRUCT__entry(
-		__string(name, lock->name)
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+		__string(name, lock->dep_map.name)
+#endif
 		__field(void *, lockdep_addr)
 	),
 
 	TP_fast_assign(
-		__assign_str(name, lock->name);
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+		__assign_str(name, lock->dep_map.name);
+#endif
 		__entry->lockdep_addr = lock;
 	),
-
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
 	TP_printk("%p %s",  __entry->lockdep_addr, __get_str(name))
+#else
+	TP_printk("%p",  __entry->lockdep_addr)
+#endif
+
 );
 
 TRACE_EVENT(mutex_acquired,
@@ -34,16 +42,23 @@ TRACE_EVENT(mutex_acquired,
 	TP_ARGS(lock, ip),
 
 	TP_STRUCT__entry(
-		__string(name, lock->name)
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+		__string(name, lock->dep_map.name);
+#endif
 		__field(void *, lockdep_addr)
 	),
 
 	TP_fast_assign(
-		__assign_str(name, lock->name);
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+		__assign_str(name, lock->dep_map.name);
+#endif
 		__entry->lockdep_addr = lock;
 	),
-
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
 	TP_printk("%p %s",  __entry->lockdep_addr, __get_str(name))
+#else
+	TP_printk("%p",  __entry->lockdep_addr)
+#endif
 );
 #endif
 #ifdef CONFIG_LOCKDEP
